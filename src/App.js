512c8fcfc1b5ec9318321dad45d6f90c2c4c2be3
thomp907 <andrewtho5942@gmail.com>
2024-07-22 14:07:03 -0400

    fix sizing for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,37 +1,6 @@
-import React, { useEffect, useState } from "react";
- 
+import React from "react";
 
 export default function App() {
-  const [scaleFactor, setScaleFactor] = useState(1);
-
-useEffect(() => {
-  const handleResize = () => {
-    console.log("resized");
-    const newScaleFactor = window.innerWidth / 1600;
-    setScaleFactor(newScaleFactor);
-  };
-
-  window.addEventListener('resize', handleResize);
-
-  return () => {
-    window.removeEventListener('resize', handleResize);
-  };
-}, []);
-
-useEffect(() => {
-  const handleOnLoad = () => {
-    console.log("init scale sized");
-    const newScaleFactor = window.innerWidth / 1600;
-    setScaleFactor(newScaleFactor);
-  };
-
-  window.addEventListener('load', handleOnLoad);
-
-  return () => {
-    window.removeEventListener('load', handleOnLoad);
-  };
-}, []);
-
   return (
     <div style={{ backgroundColor: '#6B7280', height: '100vh', overflow: 'hidden' }}>
       <iframe
@@ -40,8 +9,9 @@ useEffect(() => {
         width="1600"
         height="800"
         src="map.html"
-        style={{ transform: `scale(${scaleFactor})`, transformOrigin: '0 0' }}
+        style={{ transformOrigin: '0 0', width: '100%', height: '100%' }}
       />
     </div>
   );
 }
+
